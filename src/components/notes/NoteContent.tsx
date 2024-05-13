ea2bc1cc641507587af841af2d8005902e2dce9c
soumakkdev <soumakkdutta@gmail.com>
2024-05-13 22:58:48 +0530

    note context menu added

--- src/components/notes/NoteContent.tsx
+++ src/components/notes/NoteContent.tsx
@@ -1,21 +1,16 @@
 'use client'
-import { useAtom, useAtomValue } from 'jotai'
-import { Star, Trash2 } from 'lucide-react'
+import { useAtom } from 'jotai'
 import { useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
-import { Tooltip } from '../ui/tooltip'
-import NoteEditor from './NoteEditor'
-import { useDeleteNote, useNote, useUpdateNote } from './Notes.query'
-import { activeNoteAtom } from './notes.utils'
-import Modal from '../widgets/Modal'
 import { Button } from '../ui/button'
+import NoteEditor from './NoteEditor'
+import { useNote, useUpdateNote } from './Notes.query'
+import { activeNoteAtom } from './notes.utils'
 
 export default function NoteContent() {
 	const [activeNote, setActiveNote] = useAtom(activeNoteAtom)
-	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
 
 	const { mutate: updateNote, isPending } = useUpdateNote(activeNote)
-	const { mutate: deleteNote } = useDeleteNote()
 	const { data, isLoading: isNoteLoading } = useNote(activeNote)
 	const [html, setHtml] = useState(data?.content)
 
@@ -28,14 +23,6 @@ export default function NoteContent() {
 				},
 			}
 		)
-	}
-
-	function handleDeleteNote(noteId: string) {
-		deleteNote(noteId, {
-			onSuccess: () => {
-				setIsDeleteModalOpen(false)
-			},
-		})
 	}
 
 	useHotkeys('mod+s', () => updateNoteContent(), {
@@ -62,30 +49,11 @@ export default function NoteContent() {
 		<div className="flex-1 overflow-auto relative">
 			<div className="flex justify-between p-4 sticky top-0 z-10 bg-white border-b">
 				<h1 className="text-xl font-bold">{data?.title}</h1>
-				<div className="flex items-center space-x-4">
-					{isPending ? <div>Saving...</div> : null}
-					{/* <Tooltip content="Mark as important">
-						<Star className="h-5 w-5 text-yellow-500" />
-					</Tooltip> */}
-					<Tooltip content="Move to trash">
-						<Trash2 className="h-5 w-5 text-red-500" onClick={() => setIsDeleteModalOpen(true)} />
-					</Tooltip>
-				</div>
+				<div className="flex items-center space-x-4">{isPending ? <div>Saving...</div> : null}</div>
 			</div>
 			<div className="max-w-4xl mx-auto my-6">
 				<NoteEditor initialValue={data?.content} onChange={(data) => setHtml(data)} />
 			</div>
-
-			<Modal open={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)}>
-				<Modal.Header title="Delete Modal" />
-				<p>Are you sure to delete this note?</p>
-				<Modal.Footer>
-					<Button variant="secondary">Cancel</Button>
-					<Button variant="default" onClick={() => handleDeleteNote(activeNote)}>
-						Confirm
-					</Button>
-				</Modal.Footer>
-			</Modal>
 		</div>
 	)
 }
--- src/components/notes/NoteListItem.tsx
+++ src/components/notes/NoteListItem.tsx
@@ -1,19 +1,59 @@
 import { NotesResponse } from '@/lib/pb-types'
 import { cn, formatDate } from '@/lib/utils'
 import { useAtom } from 'jotai'
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from '../ui/context-menu'
 import { activeNoteAtom } from './notes.utils'
+import { Trash2 } from 'lucide-react'
+import Modal from '../widgets/Modal'
+import { useDeleteNote } from './Notes.query'
+import { useState } from 'react'
+import { Button } from '../ui/button'
 
 export default function NoteListItem({ note }: { note: NotesResponse }) {
 	const [activeNote, setActiveNote] = useAtom(activeNoteAtom)
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+	const { mutate: deleteNote } = useDeleteNote()
+
+	function handleDeleteNote(noteId: string) {
+		deleteNote(noteId, {
+			onSuccess: () => {
+				setIsDeleteModalOpen(false)
+			},
+		})
+	}
+
 	return (
-		<div
-			className={cn('p-4 border border-transparent cursor-pointer rounded-xl hover:border-border hover:bg-muted', {
-				'!bg-primary-surface !border-primary ': activeNote === note.id,
-			})}
-			onClick={() => setActiveNote(note.id)}
-		>
-			<p className="font-semibold mb-1">{note.title}</p>
-			<p className="text-xs text-muted-foreground">Last updated on {formatDate(note.updated)}</p>
-		</div>
+		<ContextMenu>
+			<ContextMenuTrigger>
+				<div
+					className={cn('p-4 border border-transparent cursor-pointer rounded-xl hover:border-border hover:bg-muted', {
+						'!bg-primary-surface !border-primary ': activeNote === note.id,
+					})}
+					onClick={() => setActiveNote(note.id)}
+				>
+					<p className="font-semibold mb-1">{note.title}</p>
+					<p className="text-xs text-muted-foreground">Last updated on {formatDate(note.updated)}</p>
+				</div>
+			</ContextMenuTrigger>
+			<ContextMenuContent className="">
+				<ContextMenuItem onClick={() => setIsDeleteModalOpen(true)}>
+					<Trash2 className="mr-2 h-4 w-4 text-red-500" />
+					Delete
+				</ContextMenuItem>
+			</ContextMenuContent>
+
+			<Modal open={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)}>
+				<Modal.Header title="Delete Modal" />
+				<p>Are you sure to delete this note?</p>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={() => setIsDeleteModalOpen(false)}>
+						Cancel
+					</Button>
+					<Button variant="default" onClick={() => handleDeleteNote(activeNote)}>
+						Confirm
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</ContextMenu>
 	)
 }
