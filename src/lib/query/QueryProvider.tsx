85143667b558d85307d217af1c818e50fc2e7801
soumakkdev <soumakkdutta@gmail.com>
2024-05-27 23:01:26 +0530

    pocketbase cleanup

--- src/lib/query/QueryProvider.tsx
+++ src/lib/query/QueryProvider.tsx
@@ -1,12 +1,9 @@
 'use client'
-import { ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import axios from 'axios'
+import { ReactNode } from 'react'
 
 const queryClient = new QueryClient()
-
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_PB_URL
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
 	return (
