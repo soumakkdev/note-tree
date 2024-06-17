8e367330e84b65380a4a6c559e966a859e273d3d
soumakkdev <soumakkdutta@gmail.com>
2024-06-17 09:36:30 +0530

    theme toggle setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import QueryProvider from '@/lib/query/QueryProvider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<QueryProvider>{children}</QueryProvider>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<QueryProvider>{children}</QueryProvider>
+				</ThemeProvider>
 			</body>
 		</html>
 	)
--- src/components/layout/SideNav.tsx
+++ src/components/layout/SideNav.tsx
@@ -1,18 +1,33 @@
 'use client'
+import { createClient } from '@/lib/supabase/client'
+import { User } from '@supabase/supabase-js'
 import { useSetAtom } from 'jotai'
 import { Notebook, Search, Star, Trash2, User2 } from 'lucide-react'
+import { useTheme } from 'next-themes'
 import { useRouter } from 'next/navigation'
 import NotesSearch from '../notes/list/NotesSearch'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
 import { isSearchMenuOpen } from './Layout.utils'
 import NavButton from './NavButton'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '../ui/dropdown-menu'
-import { User } from '@supabase/supabase-js'
-import { createClient } from '@/lib/supabase/client'
 
 export default function SideNav({ user }: { user: User }) {
 	const router = useRouter()
 	const setIsSearchOpen = useSetAtom(isSearchMenuOpen)
 	const supabase = createClient()
+	const { theme, setTheme } = useTheme()
 
 	async function handleLogout() {
 		await supabase.auth.signOut()
@@ -38,7 +53,30 @@ export default function SideNav({ user }: { user: User }) {
 						<DropdownMenuLabel>
 							<p className="text-xs text-muted-foreground font-normal">{user.email}</p>
 						</DropdownMenuLabel>
-						<DropdownMenuItem className="text-xs">Profile</DropdownMenuItem>
+
+						<DropdownMenuSeparator />
+
+						<DropdownMenuSub>
+							<DropdownMenuSubTrigger>
+								<span className="text-xs">Theme</span>
+							</DropdownMenuSubTrigger>
+							<DropdownMenuPortal>
+								<DropdownMenuSubContent>
+									<DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+										<DropdownMenuRadioItem className="text-xs" value="system">
+											System
+										</DropdownMenuRadioItem>
+										<DropdownMenuRadioItem className="text-xs" value="dark">
+											Dark
+										</DropdownMenuRadioItem>
+										<DropdownMenuRadioItem className="text-xs" value="light">
+											Light
+										</DropdownMenuRadioItem>
+									</DropdownMenuRadioGroup>
+								</DropdownMenuSubContent>
+							</DropdownMenuPortal>
+						</DropdownMenuSub>
+
 						<DropdownMenuItem className="text-xs" onClick={handleLogout}>
 							Log out
 						</DropdownMenuItem>
